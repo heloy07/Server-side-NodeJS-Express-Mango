56473077c8f7957f73989966bc425ad5331e37d5
Heloy <heloy.leal@hotmail.com>
2020-10-09 14:10:34 +0200

    passport facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '337224480894664',
+        clientSecret: '5a44238c2c6ca49dd4dfe1ed8b9bb27b'
+    }
 }
